54664597e947c90e5f9961db874c0b37beab6915
ryan :) <rfk27@mail.aub.edu>
2022-10-09 18:15:33 +0300

    added comments and specifiactions

--- hamming_distance.c
+++ hamming_distance.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+// Function to calculate the hamming distance between two strings
+// Takes 2 prositve strings as input 
 int hamming_distance(int x, int y) {
     int z = x ^ y;
     int count = 0;
@@ -12,6 +13,12 @@ int hamming_distance(int x, int y) {
     return count;
 }
 
+// Main function
+// Runs a loop as long as the user wants to continue
+
+// 0 1 should give 1
+// 0 3 should give 2
+// 1 3 should give 1
 int main(){
     int x =1;
     int y=1;
@@ -20,6 +27,10 @@ int main(){
     printf("Enter two numbers (or a negative number to stop): ");
     scanf("%d %d", &x, &y);
     printf("\n");
+    if (y<0){
+        printf("Needs to be a positive number, try again");
+        break;
+    }
     printf("Hamming distance: %d\n", hamming_distance(x, y));
     printf("\n");
 
